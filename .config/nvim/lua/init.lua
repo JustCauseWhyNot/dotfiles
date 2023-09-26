49188639b7bb662df343dc929beb0fec4938e917
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2023-09-25 22:51:10 -0600

    improvement: lua stuff.

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -31,29 +31,126 @@ require("lazy").setup({
 },
 
 {
-  'projekt0n/github-nvim-theme',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('github-theme').setup({
-      -- ...
-    })
-
-    vim.cmd('colorscheme github_dark_colorblind')
-  end,
-},
-
-
-{
 
         "nvim-lualine/lualine.nvim",
 },
 
 {
-        { "lukas-reineke/indent-blankline.nvim" },
+        "lukas-reineke/indent-blankline.nvim"
+},
+
+
+{
+        'projekt0n/github-nvim-theme',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+                require('github-theme').setup({
+                -- ...
+                })
+
+        vim.cmd('colorscheme github_dark_colorblind')
+        end,
+},
+
+{
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = { 
+                'hrsh7th/cmp-nvim-lsp',
+                'hrsh7th/cmp-buffer',
+                },
+        config = function()
+        require'cmp'.setup {
+        snippet = {
+                expand = function(args)
+                        require'luasnip'.lsp_expand(args.body)
+                        end
+                        },
+
+                }
+        end,
+
+
+},
+
+{
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+        dependencies = {
+                'rafamadriz/friendly-snippets',
+                'saadparwaiz1/cmp_luasnip',
+                },
 },
 
 })
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
+require("cmp").setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+
+    sources = cmp.config.sources({
+      { name = 'luasnip' }, -- For luasnip users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
 
 local function getWords()
         return vim.fn.wordcount().words
