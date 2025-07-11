e586d173d9eb9394e61d394b604a936a26dde498
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2025-07-11 16:19:09 -0600

    improvement: made treesitter work better

--- .config/nvim/lua/plugins/nvim-treestitter.lua
+++ .config/nvim/lua/plugins/nvim-treestitter.lua
@@ -1,5 +1,15 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	version = false, -- last release is way too old and doesn't work on Windows
-	build = ":TSUpdate"
+	build = ":TSUpdate",
+	config = function ()
+		local configs = require("nvim-treesitter.configs")
+
+	configs.setup({
+		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+		sync_install = false,
+		highlight = { enable = true },
+		indent = { enable = true },
+	})
+end
 }
